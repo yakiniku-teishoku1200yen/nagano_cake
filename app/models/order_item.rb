61f0f4d3bd33a4ff85c0ce71b4f91b48d4ccd575
EC2 Default User <ec2-user@ip-172-31-21-250.us-west-1.compute.internal>
2022-08-23 07:06:13 +0000

    modified:   app/models/order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-enum crafting_status: { "製作不可": 0, "製作待ち": 1, "製作中": 2, "製作完了": 3 }
+enum crafting_status: { production_not_allowed: 0, waiting_for_production: 1, production: 2, production_completed: 3 }
 end
