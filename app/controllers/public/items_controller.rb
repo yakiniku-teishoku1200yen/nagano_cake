68bf3221d39606d9c4743a4b098447ceee2a2aca
kurom21 <stussy2060@gmail.com>
2022-08-23 03:50:27 +0000

    0823統合

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,18 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
+    @total_items = Item.all
+    @items = Item.page(params[:page]).per(8)
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+
+  private
+  def items_params
+    parmas.require(:item).permit(:genre_id, :name, :explanation, :price, :is_ordered, :image)
+  end
+
 end
