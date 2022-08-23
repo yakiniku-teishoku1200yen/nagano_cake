abbe45d6599c5d53365711817e68a1da16e8c53b
EC2 Default User <ec2-user@ip-172-31-21-250.us-west-1.compute.internal>
2022-08-23 03:49:47 +0000

    ゲムファイル　オーダーモデル　ルート

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,8 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 
-  enum status: { "Waiting_for_payment": 0, "payment_confirmation": 1, "In_production": 2, "Preparing_to_ship": 3, "sent": 4 }
+  enum status: { Waiting_for_payment: 0, payment_confirmation: 1, In_production: 2, Preparing_to_ship: 3, sent: 4
+
 end
+
 
