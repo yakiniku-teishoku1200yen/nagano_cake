455ceaced30265f4600c706ff65e315fa881ccd3
EC2 Default User <ec2-user@ip-172-31-21-250.us-west-1.compute.internal>
2022-08-24 03:36:17 +0000

    date

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,7 +11,7 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order_items = @order.order_items   
+    @order_items = @order.order_items
     if @order.update(order_params)
       if  params[:order][:status] == "payment_confirmation"#
         @order_items.each do |order_item|
